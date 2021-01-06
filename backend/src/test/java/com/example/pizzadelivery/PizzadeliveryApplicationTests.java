ddc14f9c9250fc3f4d451c759eae19208ddcf082
thiagomarqs <thiagodsmarques@hotmail.com>
2021-01-06 09:34:33 -0300

    Initial setup

---
+++ backend/src/test/java/com/example/pizzadelivery/PizzadeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzadeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
